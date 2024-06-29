46541d045d03e1804f88b0c882d6320e7fe438ba
yedsod <104737131+yedsod@users.noreply.github.com>
2024-06-29 17:00:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
+# Check if THREAD environment variable is set
+if [ -z "$THREAD" ]; then
+    cpu_threads=$(grep -c '^processor' /proc/cpuinfo)
+else
+    cpu_threads=$THREAD
+fi
+
 echo "Start SRBMiner-MULTI with Parameters: --algorithm verushash --pool ap.luckpool.net:3956 --wallet RQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT --cpu-threads $cpu_threads"
 
 # Run the SRBMiner-MULTI in the background with nohup
-./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT --api-enable --api-port 80
+./SRBMiner-MULTI --algorithm verushash --pool ap.luckpool.net:3956 --wallet RQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT --cpu-threads $cpu_threads --api-enable --api-port 80
